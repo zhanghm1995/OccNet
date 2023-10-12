5821f8d31ded6560c8c9f47578ef6b1d945f7a6d
zhanghm1995 <zhanghm_1995@qq.com>
2023-10-12 16:30:19 +0800

    update gitignore

--- tools/train.py
+++ tools/train.py
@@ -216,7 +216,7 @@ def main():
         test_cfg=cfg.get('test_cfg'))
     model.init_weights()
 
-    logger.info(f'Model:\n{model}')
+    # logger.info(f'Model:\n{model}')
     datasets = [build_dataset(cfg.data.train)]
     if len(cfg.workflow) == 2:
         val_dataset = copy.deepcopy(cfg.data.val)
